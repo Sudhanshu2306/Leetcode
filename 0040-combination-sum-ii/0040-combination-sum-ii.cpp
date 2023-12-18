5014b03fa3404d336dbeb91a77a391aca244cb61
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2023-12-19 02:05:43 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (45.07%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,28 +1,30 @@
 class Solution {
-private:
-    void backtrack(vector<vector<int>>& res, vector<int>& curr, int curr_sum, vector<int>& candidates, int target, int start) {
-        if(curr_sum > target) return;
-        if(curr_sum == target) {
-            res.push_back(curr);
+public:
+    void recursion(vector<int> &ans, int index, int target, vector<int>&candidates, vector<vector<int>> &res){
+        if(target==0){
+            res.push_back(ans);
             return;
         }
-
-        for(int i = start; i < candidates.size(); i++) {
-            if (i > start && candidates[i] == candidates[i - 1])
-                continue; // Skip duplicates
-            curr.push_back(candidates[i]);
-            curr_sum += candidates[i];
-            backtrack(res, curr, curr_sum, candidates, target, i+1);
-            curr.pop_back();
-            curr_sum -= candidates[i];
+        
+        for(int i=index;i<candidates.size();i++){
+            if(i>index && candidates[i-1]==candidates[i]){
+                continue;
+            }
+            if(candidates[i]>target){
+                break;
+            }
+            ans.push_back(candidates[i]);
+            recursion(ans,i+1,target-candidates[i],candidates,res);
+            ans.pop_back();
         }
+        
     }
-public:
+    
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
+        sort(candidates.begin(),candidates.end());
         vector<vector<int>> res;
-        vector<int> curr;
-        backtrack(res, curr, 0, candidates, target, 0);
+        vector<int> ans;
+        recursion(ans,0,target,candidates,res);
         return res;
     }
 };

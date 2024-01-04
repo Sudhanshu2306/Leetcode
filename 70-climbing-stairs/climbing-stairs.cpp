846f45ac3a7e309b050afc38ae4fd58e89885651
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-01-04 23:04:07 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (58.23%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        // base case
+        if(n==0 || n==1){
+            return 1;
+        }
+        int prev=1; int curr=1;
+        
+        for(int i=2;i<=n;i++){
+            //tabulation
+            int temp=curr;
+            curr=prev+curr;
+            prev=temp;
+        }
+        return curr;
+    }
+};

5469335697ada379f901558d135667323e1090d2
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-01-08 18:49:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (70.59%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    // space can be optimised using 2 variables, rather than using a vector!
+    // something like this:
+    
     int rob(vector<int>& nums) {
     // first and last cannot be together, so lets consider two subsets of array, one not having 1st element and the other not having last element.
         int n=nums.size();
@@ -7,6 +10,44 @@ public:
             return nums[0];
         vector<int> dp(n);
         // excluding 1st element
+        int pa1=0;
+        int pa2=0;
+        for(int i=1;i<n;i++){
+            int pick;
+            if(i-2<0)
+                pick=nums[i];
+            else
+                pick=nums[i]+pa2;
+            
+            int notpick=pa1+0;
+            
+            pa2=pa1;
+            pa1=max(pick,notpick);
+        }
+        int ans1=pa1;
+        
+        //excluding last element
+        int pb1=nums[0];
+        int pb2=0;
+        for(int i=1;i<n-1;i++){
+            int pick;
+            if(i-2<0)
+                pick=nums[i];
+            else
+                pick=nums[i]+pb2;
+            
+            int notpick=pb1+0;
+            
+            pb2=pb1;
+            pb1=max(pick,notpick);
+        }
+        int ans2=pb1;
+        return max(ans1,ans2);
+    }
+};
+
+/* 
+       tabulated solution 
         dp[0]=0;
         for(int i=1;i<n;i++){
             int pick;
@@ -36,8 +77,7 @@ public:
             dp[i]=max(pick,notpick);
         }
         int ans2=dp[n-2];
-        return max(ans1,ans2);
-    }
-};
+        
+*/
 
-// still space can be optimised using 2 variables, rather than using a vector!
+        

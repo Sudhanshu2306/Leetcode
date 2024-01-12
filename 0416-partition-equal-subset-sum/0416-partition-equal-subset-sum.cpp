ae6eaba86bf9de5cde833a8beb619e4cb7e5669d
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-01-12 20:17:20 +0530

    Time: 542 ms (23.20%), Space: 13.4 MB (75.21%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    // top down approach (recursion + memoisation)
     bool solve(int ind, int target, vector<int> & nums, vector<vector<int>> &dp){
         if(target==0) return true;
         
@@ -20,10 +20,29 @@ public:
         for(auto i:nums){
             sum+=i;
         }
+        if(n==1) return false;
+        if(n==2) return (nums[0]==nums[1]);
         if(sum%2==1) return false;
         else sum/=2;
         
-        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
-        return solve(n-1,sum,nums,dp);
+        vector<vector<bool>> dp(n,vector<bool>(sum+1,false));
+        // return solve(n-1,sum,nums,dp);
+        
+        // base-case 
+        for(int i=0;i<n;i++)
+            dp[i][0] = true;
+        
+        dp[0][nums[0]]=true;
+        
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=sum;target++){
+                bool notpick=dp[i-1][target];
+                bool pick=false;
+                if(target>=nums[i]) pick=dp[i-1][target-nums[i]];
+                
+                dp[i][target]=pick || notpick;
+            }
+        }
+        return dp[n-1][sum];
     }
 };

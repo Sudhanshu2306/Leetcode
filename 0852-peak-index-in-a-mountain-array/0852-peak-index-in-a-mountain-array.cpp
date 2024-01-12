376a1df576ac4e1e75292622c88463ae17f4db3b
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-01-12 20:39:04 +0530

    Time: 93 ms (34.19%), Space: 60.1 MB (39.57%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -6,8 +6,8 @@ public:
         
         while(s<e){
             int mid=s+(e-s)/2;
-            
-            if(arr[mid]<arr[mid+1])
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1] && mid+1<arr.size() && mid-1>=0) return mid;
+            else if(arr[mid]<arr[mid+1])
                 s=mid+1;
             else
                 e=mid;

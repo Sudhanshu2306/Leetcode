5579a14f5ca08b26789967c80b649fdf0aef5c29
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-01-19 18:46:25 +0530

    Time: 10 ms (67.24%), Space: 14.1 MB (16.31%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -23,7 +23,25 @@ public:
     
     int minDistance(string s, string t) {
         int n=s.length(); int m=t.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,s,t,dp);
+        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        // return solve(n-1,m-1,s,t,dp);
+        
+        // base case
+        for(int i=0;i<=m;i++) dp[0][i]=(i);
+        for(int i=0;i<=n;i++) dp[i][0]=(i);
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]) dp[i][j] = dp[i-1][j-1];
+                else{
+                    int x = dp[i][j-1] +1;
+                    int y = dp[i-1][j]+1;
+                    int z= dp[i-1][j-1]+1;
+                    
+                    dp[i][j] = min(x,min(y,z));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };

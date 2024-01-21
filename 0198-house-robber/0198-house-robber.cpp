21eb149f57a2d0661a1f5428e7621318b8520fef
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-01-21 13:09:33 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    // memoisation ( top-down approach )     
-    int solve(int index, vector<int> &nums, vector<int> &dp){
-        if(index==0){
-            return nums[index];
-        }
-        if(index<0){
-            return 0;
-        }
-        if(dp[index]!=-1) return dp[index];
+    
+    int recursion(int ind, vector<int>& nums, vector<int> &dp){
+        // base case
+        if(ind==0) return nums[0];
         
-        int pick=solve(index-2,nums,dp)+nums[index];
-        int notpick=solve(index-1,nums,dp) + 0;
+        if(ind<0) return 0;
         
-        return dp[index]=max(pick,notpick);
+        if(dp[ind]!=-1) return dp[ind];
+        
+        int notpick = recursion(ind-1,nums,dp)+0;
+        int pick = recursion(ind-2,nums,dp)+nums[ind];
+        
+        return dp[ind] = max(pick,notpick);
     }
     
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        // return solve(n-1,nums,dp);
+        vector<int> dp(n,0);
+        // return recursion(n-1,nums,dp);
+        
+        // basecase
         dp[0]=nums[0];
-        int prev=nums[0];
-        int prev2=0;
+        
         for(int i=1;i<n;i++){
             int pick;
-            // note to self: a tricky part is for index getting a negative value, just pick the current index element value, we cannot add dp[of some negative number]!             
-            if(i>1)
-                pick=prev2+nums[i];
-            else{
-                pick=nums[i];
-            }
-            int notpick=prev+0;
-            int temp=max(pick,notpick);
-            prev2=prev;
-            prev = temp;
+            if(i>1) pick=dp[i-2] +nums[i];
+            else pick = nums[i];
+            int notpick = dp[i-1];
+            
+            dp[i]=max(pick,notpick);
         }
-        return prev;
+        return dp[n-1];
     }
 };

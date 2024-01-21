305aa5f3da1e5fd8fc1f44f0c1c18d5dbe4b50d7
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-01-21 16:05:01 +0530

    Time: 444 ms (48.61%), Space: 209.5 MB (31.60%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -17,7 +17,22 @@ public:
     
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return f(0,1,2,prices,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        // return f(0,1,2,prices,dp);
+        
+        // base case
+        // we could have omitted base case, as all values are zero, but this is just for sense of understanding that how we write base case
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                for(int cap=1;cap<3;cap++){
+                    // buy
+                    if(buy==1) dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap] ,dp[ind+1][1][cap]);
+                    // sell
+                    else dp[ind][buy][cap] = max(prices[ind]+ dp[ind+1][1][cap-1] , dp[ind+1][0][cap]);
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };

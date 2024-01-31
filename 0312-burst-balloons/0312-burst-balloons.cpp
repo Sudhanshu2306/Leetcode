8e0b0bee28c0e430783ff935daa86258819c1943
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-01-31 20:50:49 +0530

    Time: 108 ms (95.16%), Space: 11.9 MB (18.3%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -14,12 +14,35 @@ public:
         return dp[i][j]=maxi;
     }
 
-    int maxCoins(vector<int>& nums) {
+    int tabulation(vector<int>&nums){
         int n=nums.size();
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        /* one more important point i is from 1 to n, j is from 1 to n, and now ind is from i to j 
+        (both inclusive) so this should be reflected in dp vector also, it should be (n+2)x(n+2). */
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
 
-        return f(1,n,nums,dp);
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                // base case here i.e. when i>j :
+                if(i>j) continue;
+                int maxi=INT_MIN;
+                for(int ind=i;ind<=j;ind++){
+                    int coins=nums[i-1]*nums[ind]*nums[j+1]+ dp[i][ind-1]+ dp[ind+1][j];
+                    maxi=max(maxi,coins);
+                }
+                dp[i][j]=maxi;
+            }
+        }
+        return dp[1][n];
+    }
+
+    int maxCoins(vector<int>& nums) {
+        // int n=nums.size();
+        // nums.insert(nums.begin(),1);
+        // nums.push_back(1);
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return f(1,n,nums,dp);
+        return tabulation(nums);
     }
 };

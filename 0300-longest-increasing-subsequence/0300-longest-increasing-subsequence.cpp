ff9606d40209ec7641c13e4e39c290f28ea7dba9
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-02-01 23:58:50 +0530

    Time: 268 ms (27.92%), Space: 294.6 MB (6.88%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -18,6 +18,21 @@ public:
         return recursion_memo(0,-1,nums,dp);
     } */
 
+    int tabulation(vector<int> &nums){
+        int n=nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev=ind-1;prev>=-1;prev--){
+                int pick=INT_MIN;
+                if(prev==-1 || nums[ind]>nums[prev]) pick = 1 + dp[ind+1][ind+1];
+                int notpick = dp[ind+1][prev+1] + 0;
+
+                dp[ind][prev+1] = max(pick, notpick);
+            }
+        }
+        return dp[0][0];
+    }
+
     int lengthOfLIS(vector<int>& arr) {
         int n=arr.size();
         vector<int> temp; temp.push_back(arr[0]);
@@ -28,7 +43,8 @@ public:
                 temp[ind]=arr[i];
             }
         }
-        return temp.size();
+        // return temp.size();
+        return tabulation(arr);
     }
 
 };

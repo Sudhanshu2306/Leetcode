99c144aee81665f30dbe1facc3c1b32aad7c1717
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-02-02 19:55:51 +0530

    Time: 3 ms (17.15%), Space: 7.5 MB (8.37%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
-    
+
+    void f(int start, int low, int high, vector<int> &ans){
+        if(start>high) return;
+        if(start>=low) ans.push_back(start);
+
+        int last=start%10;
+        if(last<9)
+            return f(start*10+(last+1),low, high, ans);
+    }
+
+    vector<int> sequentialDigits(int low, int high){
+        vector<int> ans;
+        for(int start=1;start<10;start++){
+            f(start,low,high,ans);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+
+    // this is pure observation, but this is more of manual work!
+    /*
     vector<int> sequentialDigits(int low, int high) {
         set<int> st = {12,23,34,45,56,67,78,89,123,234,345,456,567,678,
         789,1234,2345,3456,4567,5678,6789,12345,23456,34567,45678,56789,123456,
@@ -14,4 +34,5 @@ public:
         }
         return ans;
     }
+    */
 };

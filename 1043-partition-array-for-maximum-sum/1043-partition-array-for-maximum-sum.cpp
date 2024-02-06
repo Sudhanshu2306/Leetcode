19017148137e66966f9185f8343a1b6d8d940239
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-02-06 16:48:20 +0530

    Time: 5 ms (86%), Space: 10.9 MB (24.3%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -20,9 +20,34 @@ public:
         return dp[ind]= maxAns;
     }
 
+    int tabulation(vector<int>& arr, int k){
+        int n=arr.size();
+        vector<int> dp(n+1,0);
+
+        // base case : already all elements are zero
+
+        for(int ind=n-1;ind>=0;ind--){
+            int maxi=INT_MIN;
+            int count=0;
+            int maxAns=INT_MIN;
+            for (int j = ind; j < std::min(ind + k, static_cast<int>(arr.size())); j++) {            
+                count++;
+                maxi=max(maxi,arr[j]);
+                int sum=maxi*count+dp[j+1];
+                maxAns=max(maxAns,sum);
+            }
+            dp[ind]= maxAns;
+        }
+        return dp[0];
+    }
+
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        /*
         int n=arr.size();
         vector<int> dp(n,-1);
         return f(0,arr,k,dp);
+        */
+        
+        return tabulation(arr,k);
     }
 };

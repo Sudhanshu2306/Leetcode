7f9b5a36643368b8c84d246c37f9a03af39c03a6
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-02-11 01:04:19 +0530

    Time: 28 ms (68.45%) | Memory: 24.5 MB (36.87%) - LeetSync

---
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
@@ -0,0 +1,37 @@
+class CustomStack {
+public:
+    vector<int> arr;
+    int s;
+    CustomStack(int maxSize) {
+        this->s=maxSize;
+    }
+    
+    void push(int x) {
+        if(arr.size()<s){
+            arr.push_back(x);
+        }
+    }
+    
+    int pop() {
+        if(arr.empty()) return -1;
+        else{
+            int temp = arr.back();
+            arr.pop_back();
+            return temp;
+        }
+    }
+    
+    void increment(int k, int val) {
+        for(int i=0;i<k && i<arr.size();i++){
+            arr[i]+=val;
+        }
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */

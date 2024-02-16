3bcf7054c3073038437b24961947fdac2633e42a
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-02-16 16:25:26 +0530

    Time: 7 ms (91.58%), Space: 22.3 MB (26.47%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int trap(vector<int>& heights) {
         int n=heights.size();
         vector<int> pre(n); pre[0]=heights[0];
@@ -15,6 +16,28 @@ public:
         for(int i=0;i<n;i++){
             if((min(pre[i],suf[i]))-heights[i]>0){
                 water+=min(pre[i],suf[i])-heights[i];
+            }
+        }
+        return water;
+    }
+    */
+
+    int trap(vector<int>& heights){
+        int n=heights.size();
+        int l=0,r=n-1;
+        int lmaxi=0,rmaxi=0;
+        int water=0;
+
+        while(l<=r){
+            if(heights[l]<=heights[r]){
+                if(heights[l]>lmaxi) lmaxi=heights[l];
+                else water+=(lmaxi-heights[l]);
+                l++;
+            }
+            else{
+                if(heights[r]>rmaxi) rmaxi=heights[r];
+                else water+=(rmaxi-heights[r]);
+                r--;
             }
         }
         return water;

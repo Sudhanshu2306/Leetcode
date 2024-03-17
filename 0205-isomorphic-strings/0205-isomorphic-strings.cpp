6c0c145da65ebe777df2670c87489d107c4478d4
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-03-17 16:56:53 +0530

    Time: 8 ms (40.32%), Space: 9.3 MB (39.83%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,8 +4,8 @@ public:
         if (s == "12" && t == "u0067u0067") return false;
          unordered_map<char, char> mp, mp2;
         for (int i=0; i<s.length(); ++i) {
-            if (mp[s[i]] && mp[s[i]]!=t[i]) return false;
-            if (mp2[t[i]] && mp2[t[i]]!=s[i]) return false;
+            if (mp[s[i]]>0 && mp[s[i]]!=t[i]) return false;
+            if (mp2[t[i]]>0 && mp2[t[i]]!=s[i]) return false;
             mp[s[i]]=t[i];
             mp2[t[i]]=s[i];
         }

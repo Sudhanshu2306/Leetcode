6858fe9ade155d4bddddc0ac6a3bf15d174b4f7d
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-03-18 13:28:59 +0530

    Time: 0 ms (100%), Space: 7 MB (89.21%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int twoEggDrop(int n) {
         int i;
-        for(i=0;i<n;i++){
+        for(i=1;i<n;i++){
             if(i*(i+1)>=2*n)break;
         }
         return i;

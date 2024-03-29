96f6cec66af243da94268f1df88e2e0b3e441258
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-03-30 02:13:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1 && nums[0]>=0) return true;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                int j=i-1;
+                bool flag=false;
+                while(j>=0){
+                    if(nums[j]>(i-j)){
+                        flag=true;
+                        break;
+                    }
+                    j--;
+                }
+                if(!flag) return false;
+            }
+        }
+        return true;
+    }
+};

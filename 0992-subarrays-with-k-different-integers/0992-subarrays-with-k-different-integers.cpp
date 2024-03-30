a24a5ff5f9d48900c165cde690c53ebfa7f9d0ec
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-03-30 17:55:51 +0530

    Time: 116 ms (40.6%), Space: 49.8 MB (56.66%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -18,6 +18,9 @@ public:
         return count;
     }
     int subarraysWithKDistinct(vector<int>& nums, int k){
+        // yahan thoda catch h, hum directly ye nahi nikal sakte ki exactly k distinct elements wale subarrays kitne 
+        // par hum ye nikal sakte h ki at most k distinct elements waale subarrays kitne h
+        // aur phir usko atleat (k-1) distinct elements waale subarrays se subtract kar denge!
         return f(nums,k)-f(nums,k-1);
     }
 };

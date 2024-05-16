28173af672c82b7d7ce65086a8b6efc2431232ef
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-05-16 21:51:32 +0530

    Time: 124 ms (53.49%) | Memory: 74.7 MB (77.92%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        // maximum length of subarray with at most 2 types of numbers.
+        unordered_map<int,int> mp;
+        int maxi=INT_MIN;
+        int n=fruits.size();
+        int i=0,j=0;
+
+        while(j<n){
+            mp[fruits[j]]++;
+            if(mp.size()>2){
+                while(mp.size()>2){
+                    mp[fruits[i]]--;
+                    if(mp[fruits[i]]==0) mp.erase(fruits[i]);
+                    i++;
+                }
+            }
+
+            if(mp.size()<=2){
+                maxi=max(maxi,j-i+1);
+            }
+            j++;
+        }
+        return maxi;
+    }
+};

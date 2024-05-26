718772cc70a03c7e898f9883c972ac833a76c5c3
Sudhanshu2306 <126086429+Sudhanshu2306@users.noreply.github.com>
2024-05-26 19:04:32 +0530

    Time: 1338 ms (13.50%) | Memory: 458.7 MB (14.59%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -17,6 +17,37 @@ int mod=1e9+7;
 
     int checkRecord(int n) {
         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(3,vector<int>(4,-1)));
-        return f(n,0,0,dp);
+        // return f(n,0,0,dp);
+
+        // base case
+        for(int i=0;i<2;i++){
+            for(int j=0;j<3;j++){
+                dp[0][i][j]=1;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<3;j++){
+                dp[i][2][j]=0;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                dp[i][j][3]=0;
+            }
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<2;j++){
+                for(int k=0;k<3;k++){
+                    long long ans=dp[i-1][j+1][0]%mod;
+                    ans+=dp[i-1][j][0]%mod;
+                    ans+=dp[i-1][j][k+1]%mod;
+
+                    dp[i][j][k]=ans%mod;
+                }
+            }
+        }
+        return dp[n][0][0]%mod;
     }
 };

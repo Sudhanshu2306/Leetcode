5a8167a183432c08c663351557fba57610d91e0f
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-06-24 17:19:20 +0530

    Time: 97 ms (36.68%) | Memory: 108.5 MB (96.72%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -3,14 +3,15 @@ public:
     int minKBitFlips(vector<int>& nums, int k) {
         int n=nums.size();
         int count=0;
-        queue<int> q;
+        int flips=0;
 
         for(int i=0;i<n;i++){
-            while(!q.empty() && i>q.front()+k-1) q.pop();
-            if((nums[i]+q.size())%2==0){
+            if(i-k>=0 && nums[i-k]==2) flips-=1;
+            if((flips+nums[i])%2==0){
                 if(i+k>n) return -1;
+                nums[i]=2;
+                flips+=1;
                 count+=1;
-                q.push(i);
             }
         }
         return count;

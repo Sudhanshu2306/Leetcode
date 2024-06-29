02036577a6b363f4fe360dad43e9de826983126b
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-06-29 18:41:33 +0530

    Time: 10 ms (77.21%), Space: 13.7 MB (25.46%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -4,29 +4,33 @@ class Solution {
 public:
     
     ll solve(int i,int j, vector<vector<int>>& matrix,vector<vector<int>>& dp){
+        // base case
         int m=matrix[0].size();
-        if(j<0 || j>=m)
-            return INT_MAX;
-        
-        if(i==0) return matrix[i][j];
+        if(j<0 || j>=m) return 1e9;
+        if(i==0) return matrix[0][j];
+
         if(dp[i][j]!=-1) return dp[i][j];
-        ll up=solve(i-1,j,matrix,dp)+matrix[i][j];
-        ll left=solve(i-1,j-1,matrix,dp)+matrix[i][j];
-        ll right=solve(i-1,j+1,matrix,dp)+matrix[i][j];
-        
-        return dp[i][j] = min(up,min(left,right));
+        int up=solve(i-1,j,matrix,dp)+matrix[i][j];
+        int ld=solve(i-1,j-1,matrix,dp)+matrix[i][j];
+        int rd=solve(i-1,j+1,matrix,dp)+matrix[i][j];
+
+        return dp[i][j]=min({up,ld,rd});
+
     }
     
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
         int ans=INT_MAX;
-        vector<vector<int>> dp(n,vector<int>(m,-1));
         
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
         // for(int i=0;i<m;i++){
-        //     ans=min(ans,solve(n-1,i,matrix,dp));
+        //     ans=min((ll)ans,solve(n-1,i,matrix,dp));
         // }
         // return int(ans);
+        
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        // base case
         
         for(int i=0;i<m;i++){
             dp[0][i]=matrix[0][i];
@@ -35,19 +39,22 @@ public:
         for(int i=1;i<n;i++){
             for(int j=0;j<m;j++){
                 ll up=dp[i-1][j]+matrix[i][j];
-                ll right,left;
+                ll right=1e9; ll left=1e9;
                 if(j>0) left=dp[i-1][j-1] +matrix[i][j];
-                else left= INT_MAX;
+                // else left= INT_MAX;
                 if(j+1<m) right=dp[i-1][j+1] +matrix[i][j];
-                else right = INT_MAX;
-                dp[i][j] = min(up,min(left,right));
+                // else right = INT_MAX;
+                dp[i][j] = min({up,left,right});
             }
         }
         
         for(int i=0;i<m;i++){
             ans=min(ans,dp[n-1][i]);
         }
-        return ans;
-        
+        return ans;  
     }
 };
+        
+        
+        
+     

5cbf0bdedc94ccf47571b86f96affdfd02154c46
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-07-02 19:19:58 +0530

    Time: 10 ms (93.41%) | Memory: 11.4 MB (71.19%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
+    int f(int ind, int prev, vector<int> &nums, vector<vector<int>> &dp){
+        if(ind>=nums.size()) return 0;
+        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
+
+        int len=0+f(ind+1,prev,nums,dp);
+        if(nums[ind]%nums[prev]==0 || prev==-1) len=max(len,1+f(ind+1,ind,nums,dp));
+
+        return dp[ind][prev+1]=len;
+    }
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,1);
-        vector<int> hash(n,0);
-        int maxi=1;
-        int lastIndex=0;
         sort(nums.begin(),nums.end());
+        vector<int> dp(n,1), hash(n,0);
+
+        // return f(0,-1,nums,dp);
+
+        int maxi=1;
+        int lastInd=0;
         for(int i=1;i<n;i++){
             hash[i]=i;
-            for(int prev=0;prev<i;prev++){
-                if(nums[i]%nums[prev]==0 && 1+dp[prev]>dp[i]){
-                    dp[i]=1+dp[prev];
-                    hash[i]=prev;
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0 && dp[i]<dp[j]+1){
+                    dp[i]=dp[j]+1;
+                    hash[i]=j;
                 }
             }
             if(dp[i]>maxi){
                 maxi=dp[i];
-                lastIndex=i;
+                lastInd=i;
             }
         }
-        vector<int> temp(maxi); temp[0]=nums[lastIndex];
-        int ind=1;
-        while(lastIndex!=hash[lastIndex]){
-            lastIndex=hash[lastIndex];
-            temp[ind++]=nums[lastIndex];
+        vector<int> ans; 
+        ans.push_back(nums[lastInd]);
+        while(lastInd!=hash[lastInd]){
+            lastInd=hash[lastInd];
+            ans.push_back(nums[lastInd]);
         }
-        reverse(temp.begin(),temp.end());
-        return temp;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };

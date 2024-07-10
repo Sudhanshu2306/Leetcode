242ceb41bdbcbb2cbd672cb708f02151a0812e36
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-07-10 15:09:00 +0530

    Time: 9 ms (22.38%) | Memory: 14.4 MB (17.13%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string> st;
+        for(auto &i:logs){
+            if(i=="../") {
+                if(!st.empty()) st.pop();
+            }
+                
+            else if(i=="./"){
+            }
+            else{
+                st.push(i);
+            }
+        }
+        return st.size();
+    }
+};

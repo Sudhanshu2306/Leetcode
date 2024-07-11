4342bb59c787848882170083af83bf687af9d164
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-07-11 08:05:08 +0530

    Time: 64 ms (61.25%) | Memory: 29.2 MB (42.10%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,6 +6,7 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
+            // this is a key step
             if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1;
             int k=n-1;
@@ -17,6 +18,7 @@ public:
                     vector<int> temp={nums[i],nums[j],nums[k]};
                     ans.push_back(temp);
                     j++; k--;
+                    // this step also counts to avoid repetation
                     while(j<k && nums[j]==nums[j-1]) j++;
                     while(j<k && nums[k]==nums[k+1]) k--;
                 } 

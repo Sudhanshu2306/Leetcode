096ea53ca963f4d0ad2538542cadb619543d1c23
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-07-13 19:42:47 +0530

    Time: 8 ms (35.62%) | Memory: 18.5 MB (10.23%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
-    // int f(int i, int j, vector<int> &piles, vector<vector<int>> dp){
-        
-    // }
+    int f(int i, int j, vector<int> &piles, vector<vector<int>> &dp){
+        // base case
+        if(i>j) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int one=piles[i]+f(i+1,j-1,piles,dp);
+        int two=piles[j]+f(i+1,j-1,piles,dp);
+
+        return dp[i][j]=max(one,two);
+
+    }
 
     bool stoneGame(vector<int>& piles) {
-        return true;
+        // return true;
+        int n=piles.size();
+        int sum=0;
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        int alice=f(0,n-1,piles,dp);
+        for(auto i:piles)sum+=i;
+
+        if(sum-alice<alice) return true;
+        else return false;
     }
 };

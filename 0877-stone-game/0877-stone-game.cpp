0b57101ed4eeb6c86a6a835817e533fac0bb7957
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-07-13 19:33:50 +0530

    Time: 4 ms (60.15%), Space: 9.6 MB (40.97%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    // int f(int i, int j, vector<int> &piles, vector<vector<int>> dp){
+        
+    // }
+
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};

63376c521501e020db7abaae2489731bf9550df1
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-07-23 14:29:29 +0530

    Time: 17 ms (53.18%), Space: 17.4 MB (21.18%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        unordered_map<int, vector<pair<int, int>>> adj;
-        for (auto& flight : flights) {
-            adj[flight[0]].push_back({flight[1], flight[2]});
+        queue<pair<int,pair<int,int>>> q;
+        // int n=flights.size();
+        vector<pair<int,int>> adj[n];
+
+        for(auto i:flights){
+            adj[i[0]].push_back({i[1],i[2]});
         }
+        // {stops,{node,dist}};
+        q.push({0,{src,0}});
 
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
+        vector<int> c(n,1e9);
+        c[src]=0;
+        while(!q.empty()){
+            auto i=q.front(); q.pop();
+            int stops=i.first;
+            int node=i.second.first;
+            int dist=i.second.second;
+            if(stops>k) continue;
 
-        queue<pair<int, int>> q;
-        q.push({src, 0});
-        int stops = 0;
+            for(auto itr:adj[node]){
+                int adjNode=itr.first;
+                int d=itr.second;
 
-        while (!q.empty() && stops <= k) {
-            int sz = q.size();
-            while (sz-- > 0) {
-                auto [node, distance] = q.front();
-                q.pop();
-
-                if (!adj.count(node)) continue;
-
-                for (auto& [neighbour, price] : adj[node]) {
-                    if (price + distance >= dist[neighbour]) continue;
-                    dist[neighbour] = price + distance;
-                    q.push({neighbour, dist[neighbour]});
+                if(dist+d<c[adjNode] && stops<=k){
+                    c[adjNode]=dist+d;
+                    q.push({stops+1,{adjNode,c[adjNode]}});
                 }
             }
-            stops++;
         }
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+        if(c[dst]==1e9) return -1;
+        return c[dst];
     }
 };

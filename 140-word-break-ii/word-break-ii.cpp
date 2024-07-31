9531f1c99007b0a5806bd5e593d1df2c33876a1b
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-08-01 01:21:19 +0530

    Time: 5 ms (6.58%) | Memory: 9.2 MB (57.24%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,54 +1,23 @@
-class TrieNode {
-public:
-    TrieNode* links[26];
-    bool flag = false;
-
-    TrieNode() {
-        for (int i = 0; i < 26; i++) {
-            links[i] = nullptr;
-        }
-    }
-};
-
 class Solution {
 public:
-    TrieNode* root;
-
-    void insert(string word) {
-        TrieNode* node = root;
-        for (int i = 0; i < word.size(); i++) {
-            if (node->links[word[i] - 'a'] == nullptr) {
-                node->links[word[i] - 'a'] = new TrieNode();
-            }
-            node = node->links[word[i] - 'a'];
+    void f(int ind, string s, vector<string>& wordDict, string temp, vector<string> &ans){
+        if(ind==s.size()){
+            ans.push_back(temp.substr(1));
+            return;
         }
-        node->flag = true;
-    }
-
-    void f(int ind, TrieNode* node, vector<string>& ans, string s, string store) {
-        if (node->flag) {
-            if (ind == s.size()) {
-                ans.push_back(store);
-                return;
+        string word="";
+        for(int i=ind;i<s.size();i++){
+            word+=s[i];
+            for(int j=0;j<wordDict.size();j++){
+                if(word==wordDict[j]){
+                    f(i+1,s,wordDict,temp+" "+word,ans);
+                }
             }
-            f(ind, root, ans, s, store + " ");
-        }
-
-        if (ind >= s.size()) return;
-        char ch = s[ind];
-        if (node->links[ch - 'a'] != nullptr) {
-            f(ind + 1, node->links[ch - 'a'], ans, s, store + ch);
         }
     }
-
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         vector<string> ans;
-        root = new TrieNode();
-
-        for (int i = 0; i < wordDict.size(); i++) {
-            insert(wordDict[i]);
-        }
-        f(0, root, ans, s, "");
+        f(0,s,wordDict,"",ans);
         return ans;
     }
 };

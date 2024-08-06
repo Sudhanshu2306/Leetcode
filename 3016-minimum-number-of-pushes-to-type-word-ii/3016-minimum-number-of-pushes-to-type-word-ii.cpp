25a6a8b3c920cc1c230a36e0b9f20f2da4feea38
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-08-06 07:56:10 +0530

    Time: 110 ms (31.08%), Space: 29.4 MB (5.55%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int minimumPushes(string s) {
+        // so here the keypad is not given to us, we have to design one 
+        // for minimum number of presses to make type the string
+
         unordered_map<char,int> mp;
-        int n=s.length();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<s.size();i++){
             mp[s[i]]++;
         }
-        vector<int> arr;
-        for(auto i:mp) arr.push_back(i.second);
-
-        sort(arr.rbegin(),arr.rend());
-        int mini=0;
-        for(int i=0;i<arr.size();i++){
-            int x=(i/8)+1;
-            mini+=x*arr[i];
+        vector<int> ans;
+        for(auto i:mp){
+            ans.push_back(i.second);
         }
-        return mini;
+        sort(ans.rbegin(),ans.rend());
+
+        int count=0;
+        for(int i=0;i<ans.size();i++){
+            int x=(i/8)+1;
+            count+=x*ans[i];
+        }
+        return count;
     }
 };

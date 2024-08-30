5e5b428ee6e2e37abac632f14650ab0632a18ea3
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-08-30 10:07:13 +0530

    Time: 127 ms (7.85%), Space: 11.2 MB (24.68%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    // most classical varialble size sliding window question
     string minWindow(string s, string t) {
+        int n=s.size(),m=t.size();
         unordered_map<char,int> mp;
-        
-        for(int i=0;i<t.length();i++){
-            mp[t[i]]++;
-        }
-        int count=mp.size();
-        string ans="";
-        int mini=INT_MAX;
-        int i=0,j=0;
-        int st=0;
-        while(j<s.length()){
-            // if(mp.find(s[j])==mp.end()) j++;
-            if(mp.find(s[j])!=mp.end()){
-                mp[s[j]]--;
-                if(mp[s[j]]==0) count--;
-            }
-            if(count==0){
-                while(count==0){
-                    if(mp.find(s[i])!=mp.end()){
-                        mp[s[i]]++;
-                        if(mp[s[i]]==1){
-                            count++;
-                            if((j-i+1)<mini){
-                                mini=(j-i+1);
-                                st=i;
-                            }
-                        }
-                    }
-                    i++;
+        for(int i=0;i<m;i++) mp[t[i]]++;
+
+        int size=1e9; int si=-1;
+        int i=0;
+        unordered_map<char,int> mpp;
+        for(int j=0;j<n;j++){
+            mpp[s[j]]++;
+
+            int f=0;
+            for(auto it:mp){
+                if(mp[it.first]>mpp[it.first]){
+                    f=1; break;
                 }
             }
-            j++;
+            if(!f){
+                while(mpp[s[i]]>mp[s[i]]){
+                    mpp[s[i]]--;
+                    if(mpp[s[i]]==0) mpp.erase(s[i]);
+                    i++;
+                }
+
+                if(size>j-i+1){
+                    size=(j-i+1);
+                    si=i;
+                }
+            }
         }
-        if(mini==INT_MAX) return "";
-        else return s.substr(st,mini);
+        if(si!=-1) return s.substr(si,size);
+        else return "";
     }
 };

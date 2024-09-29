7aa98e548c48802ef9390ce0d100cb064f8af8b6
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2024-09-29 13:02:26 +0530

    Time: 8 ms (72.73%) | Memory: 11.7 MB (54.55%) - LeetSync

--- 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -9,46 +9,31 @@ public:
         for(auto i:vowels) x+=i.second;
         return x;
     }
-    int ss(string s, int k){
-        unordered_map<char,int> mp;
+    long long ss(string s, int k){
+        unordered_map<char,long long> vowels;
         int n=s.size();
-        int cons=0; int ans=0; int af=0;
+        long long cons=0;
+        long long ans=0;
         int i=0,j=0;
-        while(j<n){
-            mp[s[j]]++;
-            if(isVowel(s[j]) && mp[s[j]]==1) af++;
-            else if(!isVowel(s[j])) cons++;
+        while(i<=j && j<n){
+            if(isVowel(s[j])) vowels[s[j]]++;
+            else cons++;
 
-            while(cons>k && i<j){
-                mp[s[i]]--;
-                if(isVowel(s[i]) && mp[s[i]]<=0) {
-                    af--;
-                }
-                else if(!isVowel(s[i])){
-                    cons--;
-                }
+            while(i<=j && cons>=k && vowels.size()==5){
+                ans+=(n-j);
+                if(isVowel(s[i])) vowels[s[i]]--;
+                else cons--;
+                if(vowels[s[i]]==0) vowels.erase(s[i]);
                 i++;
             }
-            if(i<j && af==5 && cons==k){
-                ans++;
-                int x=i;
-                char temp=s[x];
-                unordered_map<char,int> mp1;
-                while(isVowel(temp) && mp[temp]>1){
-                    ans++;
-                    mp1[temp]++;
-                    mp[temp]--; x++;
-                    temp=s[x];
-                }
-                for(auto it:mp1) mp[it.first]+=it.second;
-            } 
+            
             j++;
         }
         return ans;
     }
     long long countOfSubstrings(string word, int k) {
-        int x1=ss(word,k);;
-        return x1;
+        long long x1=ss(word,k+1); long long x2=ss(word,k);
+        return (x2-x1);
 
     }
 };

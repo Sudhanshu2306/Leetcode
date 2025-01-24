83040cc633dd94249c102769c3628d563fbb9dee
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-01-25 02:12:52 +0530

    Time: 354 ms (70.55%) | Memory: 192.1 MB (39.88%) - LeetSync

---
+++ 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    int minMaxSums(vector<int>& nums, int k) {
+        // nCk=(n-1)Ck+(n-1)C(k-1);
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+
+        vector<vector<int>> ncr(n+1,vector<int>(k+1,1));
+        // base case
+        for(int i=0;i<=n;i++) ncr[i][1]=i,ncr[i][0]=1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=k && j<i;j++){
+                ncr[i][j]=(ncr[i-1][j]+ncr[i-1][j-1])%mod;
+            }
+        }
+
+        long long mini=0;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=min(n,k);j++){
+                if(n-i-1>=j-1) mini=(mini+(1ll*nums[i]*ncr[n-i-1][j-1]))%mod;
+            }
+        }
+
+        long long maxi=0;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=min(n,k);j++){
+                if(i>=j-1) maxi=(maxi+(1ll*nums[i]*ncr[i][j-1]))%mod;
+            }
+        }
+        long long res=(mini+maxi)%mod;
+        return res;
+    }
+};

e27a18808e2825d5048f9dc6da8c4c3a5570c61d
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-03-03 19:13:10 +0530

    Time: 357 ms (17.65%) | Memory: 34.5 MB (100.00%) - LeetSync

---
+++ 3776-find-minimum-cost-to-remove-array-elements/find-minimum-cost-to-remove-array-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int f(int i, int j, vector<int> &dp, vector<int> &a){
+        if(j==a.size()) return a[i]; //only a[i] is remaining
+        if(j==a.size()-1) return max(a[i],a[j]); // only a[i] && a[j] are remaining
+        if(dp[i]!=-1) return dp[i];
+        
+        int x=max(a[i],a[j])+f(j+1,j+2,dp,a);
+        int y=max(a[i],a[j+1])+f(j,j+2,dp,a);
+        int z=max(a[j],a[j+1])+f(i,j+2,dp,a);
+        
+
+        return dp[i]=min({x,y,z});
+
+    }
+    int minCost(vector<int>& nums) {
+        // dp problem for sure
+        // we can define dp state dp(i,j) as i represents the last element not removed 
+        // and j represents the current starting index
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        int ans=f(0,1,dp,nums);
+        return ans;
+    }
+};

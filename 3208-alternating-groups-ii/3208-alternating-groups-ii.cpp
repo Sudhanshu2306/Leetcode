638833c22914a3fc2ae1d8abc6cd98d2c905ba1c
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-03-09 09:59:14 +0530

    Time: 81 ms (19.87%), Space: 117 MB (5.23%) - LeetHub

--- 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
@@ -7,29 +7,12 @@ public:
 
         for(int j=0;j<k-1;j++) temp.push_back(colors[j]);
 
-        int count = 0;
+        int count=0;
         int m=temp.size();
-
-        for(int i=0;i<m;i++) {
-            int x=1;
-            // cout<<count<<" ";
-            for(int j=i+1;j<m;j++){
-                if(temp[j] != temp[j-1]) x++;
-
-                else{
-                    // cout<<i<<" ";
-                    i=j-1;
-                    break;
-                }
-
-                if(j==m-1){
-                    i=3*m;
-                    break;
-                }
-            }
-            // cout<<count<<" ";
-            count+=max(0,x+1-k);
-            // cout<<count<<" ";
+        int i=0;
+        for(int j=0;j<m;j++){
+            if(j>0 && temp[j]==temp[j-1]) i=j;
+            if(j-i+1>=k) count++;
         }
         return count;
     }

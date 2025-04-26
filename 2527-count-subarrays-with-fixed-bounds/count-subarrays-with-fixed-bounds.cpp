57eadd5f1ddd92c26ab1f75eed0e9215a88c64a5
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-04-26 23:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 93.9 MB (62.97%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
         int n=nums.size();
-        int notK=-1;
-        int maxi=-1, mini=-1;
         long long count=0;
 
+        // okay if I get a maximum greater than maxK or a minimum less than minK, we cannot continue with same subarray
+        int maxi=-1,mini=-1;
+        int start=-1;
+
         for(int i=0;i<n;i++){
-            if(nums[i]>maxK || nums[i]<minK) notK=i;
+            if(nums[i]<minK || nums[i]>maxK) start=i;
             if(nums[i]==maxK) maxi=i;
             if(nums[i]==minK) mini=i;
 
-            count+=(max(min(maxi,mini)-notK,0));
+            if(maxi!=-1 && mini!=-1) count+=max(min(maxi,mini)-start,0);
         }
+
         return count;
     }
 };

9d6c9b72e53d94af8d3cea99e811f7f2e03ede49
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-04-29 18:15:03 +0530

    Time: 6 ms (49.21%) | Memory: 121.7 MB (48.26%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
-        int x=*max_element(nums.begin(),nums.end());
-        // return x;
         long long count=0;
-        int i=0,j=0;
-        long long ans=0;
         int n=nums.size();
+
+        int i=0,j=0;
+        long long maxi=*max_element(nums.begin(),nums.end());
+        long long temp=0;
         while(j<n){
-            if(nums[j]==x){
-                count++;
-            } 
-            if(count>=k){
-                ans+=(n-j);  // why?? kyuki bhai aage ke saare elements ko add karenge phir bhi valid hoga
-                while(i<=j && count>=k){
-                    if(nums[i]==x) count--;
-                    if(count>=k) ans+=(n-j);
+            if(nums[j]==maxi) temp++;
+            if(temp>=k){
+                count+=((n-1-j)+1);
+                while(i<=j && temp>=k){
+                    if(nums[i]==maxi) temp--;
+
+                    if(temp>=k) count+=((n-1-j)+1);
                     i++;
                 }
             }
             j++;
         }
-        return ans;
+        return count;
     }
 };

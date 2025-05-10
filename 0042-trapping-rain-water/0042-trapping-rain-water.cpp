3ae65c37ed47e256a05deabd8b8dcf4131b3ce5d
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-05-10 12:40:02 +0530

    Time: 0 ms (100%), Space: 26.1 MB (50.16%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
-    /*
-    int trap(vector<int>& heights) {
-        int n=heights.size();
-        vector<int> pre(n); pre[0]=heights[0];
-        vector<int> suf(n); suf[n-1]=heights[n-1];
+    int trap(vector<int>& a) {
+        int n=a.size();
+        int ans=0;
+        int lmax=0,rmax=0;
+        int i=0,j=n-1;
 
-        for(int i=1;i<n;i++){
-            pre[i]=max(pre[i-1],heights[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            suf[i]=max(suf[i+1],heights[i]);
-        }
-        int water=0;
-        for(int i=0;i<n;i++){
-            if((min(pre[i],suf[i]))-heights[i]>0){
-                water+=min(pre[i],suf[i])-heights[i];
-            }
-        }
-        return water;
-    }
-    */
-
-    int trap(vector<int>& heights){
-        int n=heights.size();
-        int l=0,r=n-1;
-        int lmaxi=0,rmaxi=0;
-        int water=0;
-
-        while(l<=r){
-            if(heights[l]<=heights[r]){
-                if(heights[l]>lmaxi) lmaxi=heights[l];
-                else water+=(lmaxi-heights[l]);
-                l++;
+        while(i<=j){
+            if(a[i]<=a[j]){
+                if(lmax>a[i]){
+                    ans+=(lmax-a[i]);
+                }
+                else lmax=a[i];
+                i++;
             }
             else{
-                if(heights[r]>rmaxi) rmaxi=heights[r];
-                else water+=(rmaxi-heights[r]);
-                r--;
+                if(rmax>a[j]){
+                    ans+=(rmax-a[j]);
+                }
+                else rmax=a[j];
+                j--;
             }
         }
-        return water;
+        return ans;
     }
 };

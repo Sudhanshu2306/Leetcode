18e6b32f141289abd4c3104fe58e8cf27d5933ea
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-05-23 16:46:53 +0530

    Time: 39 ms (70.74%) | Memory: 80.1 MB (43.77%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,48 +1,57 @@
+class Node{
+    public:
+    vector<Node*> links;
+    bool flag;
+    Node(){
+        links.resize(26,nullptr);
+        this->flag=false;
+    }
+};
+class Trie{
+    public:
+    Node* root;
+    Trie(){
+        root=new Node();
+    }
+
+    void insert(string x){
+        Node* curr=root;
+        for(auto it:x){
+            if(curr->links[it-'a']==nullptr) curr->links[it-'a']=new Node();
+            curr=curr->links[it-'a'];
+        }
+        curr->flag=true;
+    }
+    int search(string x){
+        Node* curr=root;
+        int i=0;
+        for(auto it:x){
+            if(curr->links[it-'a']==nullptr) return -1;
+            curr=curr->links[it-'a'];
+            if(curr->flag) return i;
+            i++;
+        }
+        return -1;
+    }
+};
+
 class Solution {
 public:
-    string replaceWords(vector<string>& dictionary, string sentence) {
-        stringstream ss(sentence);
-        string token;
-        vector<string> sent;
-        char delimiter = ' ';
-        while( getline(ss,token,delimiter)){
-            sent.push_back(token);
-        }
-        
+    string replaceWords(vector<string>& d, string s) {
+        Trie t;
+        for(auto it:d) t.insert(it);
+
+        stringstream ss(s);
+        string word;
         string ans="";
-        for(int i =0 ;i < sent.size(); i++){
-          string word = sent[i];
-          
-          bool flag = 1;
-          string s ="";
-          int minidx = INT_MAX;
-          for(auto &str : dictionary){
-            int idx = 0;
-            flag = 1;
-            
-            
-            while(idx < str.size()){
-                if(str.size() > word.size()){
-                    flag = 0;
-                    break;
-                }
-              if(word[idx] != str[idx]){
-                flag = 0;
-                break;
-              }
-              idx++; 
-            }
-            
-             if( flag && minidx > str.size()){
-                minidx = str.size();
-                s = str;
-            }
-          }
-          if( s.size() == 0)ans += word;
-          else ans += s;
-         
-          if(i < sent.size()-1)ans += " ";
+        while(ss>>word){
+            int i=t.search(word);
+            string y=word.substr(0,i+1);
+            if(i!=-1) ans+=y; 
+            else ans+=word;
+            ans+=" ";
         }
-        return ans;
+        int n=ans.size();
+        return ans.substr(0,n-1);
     }
 };

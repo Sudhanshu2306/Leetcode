d2b8d9261c8eb5d796132d7368246b5a227cecc6
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-05-27 16:07:26 +0530

    Time: 3 ms (28.55%) | Memory: 60.9 MB (25.26%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -4,19 +4,20 @@ public:
         int n=nums.size();
         int maxi=0;
         int zero=0;
-        priority_queue<int, vector<int>, greater<int>> pq;
+        // priority_queue<int, vector<int>, greater<int>> pq;
+        queue<int> q;
 
         int i=0,j=0;
-        // simple sa logic h, k 0's ko 1 banana h, ab isko aaise dekho ki maximum sub-array with k zeros
+        // simple sa logic h, k 0's ko 1 banana h, ab isko aaise dekho ki maximum sub-array sum with k zeros
         while(j<n){
             if(nums[j]==0){
                 zero++;
-                pq.push(j);
+                q.push(j);
             } 
             if(zero<=k) maxi=max(maxi,j-i+1);
             else{
-                i=pq.top()+1;
-                pq.pop();
+                i=q.front()+1;
+                q.pop();
                 zero--;
             }
             j++;

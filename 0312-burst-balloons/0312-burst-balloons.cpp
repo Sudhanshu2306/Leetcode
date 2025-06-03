0cdd68d35970bdca2bf08b89e3fc88d93a0526a8
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-06-03 19:40:59 +0530

    Time: 202 ms (39.21%), Space: 13.8 MB (80.74%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-
+    /* this is a typical problem, reason because the approach is very unque to it, first we have
+    to add 1 and 1 at each end to make sure at end of boundary 1 is multiplied. now once that is done,
+    we start the problem thinking that the last balloon burst was some, and then adding any other balloon
+    that was burst previously.*/
+    
     int f(int i, int j, vector<int> &nums, vector<vector<int>> &dp){
         if(i>j) return 0;
 
         if(dp[i][j]!=-1) return dp[i][j];
 
-        int maxi=INT_MIN;
+        int maxi;
         for(int ind=i;ind<=j;ind++){
-            int coins=nums[i-1]*nums[ind]*nums[j+1]+ f(i,ind-1,nums,dp)+ f(ind+1,j,nums,dp);
-            maxi=max(maxi,coins);
+            int ans=nums[i-1]*nums[ind]*nums[j+1]+f(i,ind-1,nums,dp)+f(ind+1,j,nums,dp);
+            maxi=max(maxi,ans);
         }
         return dp[i][j]=maxi;
     }
@@ -18,7 +22,7 @@ public:
         int n=nums.size();
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        /* one more important point i is from 1 to n, j is from 1 to n, and now ind is from i to j 
+        /* one more important point, i is from 1 to n, j is from 1 to n, and now ind is from i to j 
         (both inclusive) so this should be reflected in dp vector also, it should be (n+2)x(n+2). */
         vector<vector<int>> dp(n+2,vector<int>(n+2,0));
 
@@ -38,11 +42,11 @@ public:
     }
 
     int maxCoins(vector<int>& nums) {
-        // int n=nums.size();
-        // nums.insert(nums.begin(),1);
-        // nums.push_back(1);
-        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        // return f(1,n,nums,dp);
-        return tabulation(nums);
+        int n=nums.size();
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return f(1,n,nums,dp);
+        // return tabulation(nums);
     }
 };

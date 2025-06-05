7b0e3245fde92beacf4b3aea51c45b2fd720f1a1
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-06-05 18:41:00 +0530

    Time: 5 ms (23.18%), Space: 22 MB (15.82%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,42 +1,18 @@
 class Solution {
 public:
-    // there is variable buy as a flag variable so that to tract wether we can buy that item or not at that index
-    // one more thing to remeber is that in this question we'll start the recursion also with bottom up
-    int f (int ind, int buy, vector<int>& prices, vector<vector<int>> &dp){
-        // base case
-        if(ind==prices.size()) return 0;
-        
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
-        int profit;
-        // buy
-        if(buy==1) profit = max(-prices[ind] + f(ind+1,0,prices,dp), f(ind+1,1,prices,dp));
-        // sell
-        else profit = max(prices[ind]+ f(ind+1,1,prices,dp), f(ind+1,0,prices,dp));
-        
-        return dp[ind][buy] = profit;
-        
+    int f(vector<int> &a, int i, int buy, vector<vector<int>> &dp){
+        if(i>=a.size()) return 0;
+        if(dp[i][buy]!=-1) return dp[i][buy];
+
+        int profit=0;
+        if(buy==0) profit=max(f(a,i+1,1,dp)-a[i],f(a,i+1,0,dp));
+        else profit=max(f(a,i+1,0,dp)+a[i],f(a,i+1,1,dp));
+
+        return dp[i][buy]=profit;
     }
-    
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        // vector<vector<int>> dp(n+1,vector<int>(2,0));
-        // return f(0,1,prices,dp);
-        
-        // base case
-        // already all index elements are zero;
-        
-        
-        double nextBuy, nextNotBuy, todayBuy, todayNotBuy;
-        nextBuy = nextNotBuy = 0;
-        for(int ind=n-1;ind>=0;ind--){
-            todayBuy = max(-prices[ind] + nextNotBuy, nextBuy);
-            
-            todayNotBuy = max(prices[ind] + nextBuy, nextNotBuy);
-            
-                
-            nextBuy=todayBuy;
-            nextNotBuy = todayNotBuy;
-        }
-        return (int)nextBuy;
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        return f(prices,0,0,dp);
     }
 };

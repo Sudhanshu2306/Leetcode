e68304b1b334a53705c249f469c90805ccf234f9
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-06-06 19:42:41 +0530

    Time: 40 ms (5.16%) | Memory: 41.3 MB (5.01%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
+    bool f(string s, int i, int count, vector<vector<int>> &dp){
+        if(count<0) return false;
+        if(i==s.size()) return count==0;
+
+        if(dp[i][count]!=-1) return dp[i][count];
+
+        if(s[i]=='(') return f(s,i+1,count+1,dp);
+        if(s[i]==')') return f(s,i+1,count-1,dp);
+
+        return dp[i][count]=f(s,i+1,count+1,dp) | f(s,i+1,count-1,dp) | f(s,i+1,count,dp);
+    }
+    // bool checkValidString(string s) {
+    //     int mini=0; int maxi=0;
+    //     for(int i=0;i<s.size();i++){
+    //         if(s[i]=='('){
+    //             mini++; maxi++;
+    //         }
+    //         else if(s[i]==')'){
+    //             mini--; maxi--;
+    //         }
+    //         else{
+    //             mini--; maxi++;
+    //         }
+    //         mini=max(mini,0);
+    //         if(maxi<0) return false;
+    //     }
+    //     if(mini==0) return true;
+    //     return false;
+    // }
+
     bool checkValidString(string s) {
-        int ans=0; int maxi=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                ans++; maxi++;
-            }
-            else if(s[i]==')'){
-                ans--; maxi--;
-            }
-            else{
-                ans--; maxi++;
-            }
-            ans=max(ans,0);
-            if(maxi<0) return false;
-        }
-        if(ans==0) return true;
-        return false;
+        int n=s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return f(s,0,0,dp);
     }
 };

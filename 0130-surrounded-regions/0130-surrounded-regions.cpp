d04829436d6706945a55b74a54e604e11b6d8005
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-06-12 20:58:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,37 +1,66 @@
-class Solution {
-    void dfs(int i, int j, vector<vector<char>> &mat, vector<vector<int>> &vis){
-        vis[i][j]=1;
-        
-        vector<int> dr={-1,0,1,0};
-        vector<int> dc={0,1,0,-1};
-        int n=mat.size();
-        int m=mat[0].size();
-        
-        for(int k=0;k<4;k++){
-            int nr=i+dr[k];
-            int nc=j+dc[k];
-            
-            if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0 && mat[nr][nc]=='O'){
-                dfs(nr,nc,mat,vis);
-            }
+class DisjointSet{
+public:
+    vector<int> parent,size;
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++) parent[i]=i;
+    }
+    int findParent(int node){
+        if(node==parent[node]) return node;
+
+        return parent[node]=findParent(parent[node]);
+    }
+    void unionBySize(int u, int v){
+        int parent_u=findParent(u);
+        int parent_v=findParent(v);
+        if(parent_u==parent_v) return;
+
+        if(size[parent_u]>size[parent_v]){
+            parent[parent_v]=parent_u;
+            size[parent_u]+=size[parent_v];
+        }
+        else{
+            parent[parent_u]=parent_v;
+            size[parent_v]+=size[parent_u];
         }
     }
+};
+class Solution {
 public:
-    void solve(vector<vector<char>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        // vector<vector<char>> ans=mat;
+    void solve(vector<vector<char>>& board) {
+        int n=board.size(), m=board[0].size();
+        DisjointSet dsu(40002);
+        int x=40001;
+        
+        int dx[]={0,1,0,-1};
+        int dy[]={1,0,-1,0};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if((i==0 || j==0 || i==n-1 || j==m-1) && mat[i][j]=='O' && vis[i][j]==0){
-                    dfs(i,j,mat,vis);
+                if(board[i][j]=='O'){
+                    if(i==0 || i==n-1 || j==0 || j==m-1){
+                        dsu.unionBySize(i*m+j,x);
+                    }
+                    else{
+                        for(int k=0;k<4;k++){
+                            int ni=i+dx[k], nj=j+dy[k];
+                            if(ni>=0 && ni<n && nj>=0 && nj<m){
+                                dsu.unionBySize(i*m+j,ni*m+nj);
+                            }
+                        }
+                    }
+                    
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && mat[i][j]=='O') mat[i][j]='X';
+                if(board[i][j]=='O'){
+                    int u=dsu.findParent(i*m+j);
+                    int v=dsu.findParent(x);
+
+                    if(u!=v) board[i][j]='X';
+                }
             }
         }
     }

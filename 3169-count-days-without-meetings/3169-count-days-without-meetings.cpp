e170557bd71c8c477fec5dc591816de699dd2d3c
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-06-14 09:13:00 +0530

    Time: 73 ms (44.16%), Space: 126.5 MB (57.3%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int countDays(int days, vector<vector<int>>& a) {
         sort(a.begin(),a.end());
-        // for(auto it:a) cout<<it[0]<<" "<<it[1]<<endl;
+        int n=a.size();
+
         int count=a[0][0]-1;
-        int e=a[0][1];
-        for(int i=1;i<a.size();i++){
-            if(a[i][0]<=e) e=max(e,a[i][1]);
-            else{
-                count+=(a[i][0]-e-1);
-                e=max(e,a[i][1]);
+        int prev=a[0][1];
+        for(int i=1;i<n;i++){
+            if(a[i][0]>prev){
+                count+=(a[i][0]-prev-1);
+                prev=max(prev,a[i][1]);
             }
-            cout<<e<<" ";
+            else{
+                prev=max(prev,a[i][1]);
+            }
         }
-        count+=(days-e);
+        count+=days-prev;
         return count;
-        // return 0;
     }
 };

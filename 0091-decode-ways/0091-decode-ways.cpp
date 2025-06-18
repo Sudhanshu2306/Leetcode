f7a41c0eff66c0c281c706c70cff8a20def55b37
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-06-18 13:12:57 +0530

    Time: 3 ms (21.04%), Space: 11.1 MB (13.35%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,36 +1,20 @@
 class Solution {
-//     dp problem, not solved, watched video, revisit again!
-private:
-    
-    int dfs(int i, const string& s, unordered_map<int, int>& dp) {
-        if (i == s.size()) {
-            return 1;  // reached the end, one way to decode
-        }
-
-        if (dp.find(i) != dp.end()) {
-            return dp[i];
-        }
-
-        if (s[i] == '0') {
-            return 0;
-        }
-
-        int res = dfs(i + 1, s, dp);
-
-        if (i + 1 < s.size() && (s[i] == '1' || (s[i] == '2' && s[i + 1] >= '0' && s[i + 1] <= '6'))) {
-            res += dfs(i + 2, s, dp);
-        }
-
-        dp[i] = res;
-        return res;
-    }
 public:
-    int numDecodings(string s) {
-        if (s.empty()) {
-            return 0;
-        }
+    int mod=1e9+7;
+    int f(int i, string s, vector<int> &dp){
+        if(i==s.size()) return 1;
 
-        unordered_map<int, int> dp;
-        return dfs(0, s, dp);
+        if(dp[i]!=-1) return dp[i];
+
+        if(s[i]=='0') return dp[i]=0;
+        int ans=f(i+1,s,dp);
+        if(i<s.size()-1 && (s[i]=='1' || s[i]=='2' && s[i+1]<='6')) ans+=f(i+2,s,dp);
+
+        return dp[i]=ans;
+    }
+    int numDecodings(string s) {
+        int n=s.size();
+        vector<int> dp(n+1,-1);
+        return f(0,s,dp);
     }
 };

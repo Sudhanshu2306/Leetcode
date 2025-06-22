cb7e061fb043dfa2653833df4eb2097543621bc6
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-06-23 00:46:55 +0530

    Time: 8 ms (86.86%), Space: 9.4 MB (76%) - LeetHub

--- 2827-number-of-beautiful-integers-in-the-range/2827-number-of-beautiful-integers-in-the-range.cpp
+++ 2827-number-of-beautiful-integers-in-the-range/2827-number-of-beautiful-integers-in-the-range.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-    int dp[11][2][6][6][20][2];
-    // {index,tight,odd,even,remainder,isLeadingZero}
+    int dp[12][6][6][2][2][20];
+    // {index,odd,even,tight,leading zero,remainder};
 
-    int f(int i, int tight, int even, int odd, int rem, int zero, int k, string &s){
+    int f(int i, int odd, int even, int tight, int zero, int rem, string &s, int k){
         if(i==s.size()){
-            if(even==odd && rem==0) return 1;
+            if(rem==0 && odd==even) return 1;
             return 0;
         }
-        if(odd>5 || even>5) return 0;
-        if(dp[i][tight][odd][even][rem][zero]!=-1) return dp[i][tight][odd][even][rem][zero];
-        int count=0;
-        int limit=tight==1?s[i]-'0':9;
-        for(int d=0;d<=limit;d++){
-            int new_tight=tight & (limit==d);
-            if(zero==0 && d==0) count+=f(i+1,new_tight,even,odd,rem,0,k,s);
-            else if(d%2==0) count+=f(i+1,new_tight,even+1,odd,(rem*10+d)%k,1,k,s);
-            else count+=f(i+1,new_tight,even,odd+1,(rem*10+d)%k,1,k,s);
-        }
-        return dp[i][tight][odd][even][rem][zero]=count;
-    }
-    int numberOfBeautifulIntegers(int low, int high, int k) {
-        string a=to_string(low-1);
-        string b=to_string(high);
-        memset(dp,-1,sizeof(dp));
-        int x=f(0,1,0,0,0,0,k,a);
-        memset(dp,-1,sizeof(dp));
-        int y=f(0,1,0,0,0,0,k,b);
 
-        return y-x;
+        if(odd>5 || even>5) return 0;
+
+        if(dp[i][odd][even][tight][zero][rem]!=-1) return dp[i][odd][even][tight][zero][rem];
+
+        int count=0;
+        int limit=tight?s[i]-'0':9;
+
+        for(int d=0;d<=limit;d++){
+            int nt=tight && (d==limit);
+            if(zero==0 && d==0) count+=f(i+1,odd,even,nt,0,rem,s,k);
+            else if(d%2==0) count+=f(i+1,odd,even+1,nt,1,(rem*10+d)%k,s,k);
+            else count+=f(i+1,odd+1,even,nt,1,(rem*10+d)%k,s,k);
+        }
+        return dp[i][odd][even][tight][zero][rem]=count;
+    }
+
+    int numberOfBeautifulIntegers(int low, int high, int k) {
+        string s=to_string(high);
+        memset(dp,-1,sizeof(dp));
+        int x=f(0,0,0,1,0,0,s,k);
+        string t=to_string(low-1);
+        memset(dp,-1,sizeof(dp));
+        int y=f(0,0,0,1,0,0,t,k);
+
+        return abs(x-y);
     }
 };

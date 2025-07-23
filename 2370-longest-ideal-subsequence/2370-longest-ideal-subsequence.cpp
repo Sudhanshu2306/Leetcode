8a113297b963e62e538868f6d48115e52ba481fe
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-07-23 10:27:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-
-    int f(string &s, int k, int i, int prev, vector<vector<int>> &dp){
+    int dp[100005][26]; // prev
+    int f(int i, int prev, string &s, int k){
         if(i>=s.size()) return 0;
 
         if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        int pick=0,notpick=0;
 
-        if(prev==-1 || abs((int)s[i]-prev-'a')<=k) pick=1+f(s,k,i+1,s[i]-'a',dp);
-        if(prev==-1) notpick=f(s,k,i+1,-1,dp);
-        else notpick= f(s,k,i+1,prev,dp);
+        int take=0, nottake;
+        if(prev==-1 || abs((s[i]-'a')-prev)<=k) take=1+f(i+1,s[i]-'a',s,k);
+        nottake=f(i+1,prev,s,k);
 
-        return dp[i][prev+1]=max(pick,notpick);
+        return dp[i][prev+1]=max(take,nottake);
     }
-
     int longestIdealString(string s, int k) {
-        int n=s.size();
-        vector<vector<int>> dp(n+1, vector<int>(27,-1));
-        return f(s,k,0,-1,dp);
+        memset(dp,-1,sizeof(dp));
+
+        return f(0,-1,s,k);
     }
 };

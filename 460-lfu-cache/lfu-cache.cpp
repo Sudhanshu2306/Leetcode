658c3743e1f8cbb6df993545e04ac9c8977d3c60
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-07-28 10:27:21 +0530

    Time: 146 ms (32.40%) | Memory: 189.4 MB (62.15%) - LeetSync

--- 460-lfu-cache/lfu-cache.cpp
+++ 460-lfu-cache/lfu-cache.cpp
@@ -1,54 +1,105 @@
-class LFUCache {
-    int capacity;
-    int minFreq;
-    unordered_map<int,pair<int,int>> keyVal;
-    unordered_map<int,list<int>> freqList;
-    unordered_map<int,list<int>::iterator> pos;
+class Node {
 public:
+    int key, val, freq;
+    Node* next;
+    Node* prev;
+    Node(int key, int val) {
+        this->key = key;
+        this->val = val;
+        this->freq = 1;
+        next = prev = nullptr;
+    }
+};
+class List{
+public:
+    int size;
+    Node* head, *tail;
+    List(){
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head->next=tail; tail->prev=head;
+        size=0;
+    }
+    
+
+    void deleteNode(Node* node) {
+        Node* prev = node->prev;
+        Node* next = node->next;
+        prev->next = next;
+        next->prev = prev;
+        size--;
+    }
+
+    void insertAfterHead(Node* node) {
+        Node* x = head->next;
+        node->next = x;
+        node->prev = head;
+        head->next = node;
+        x->prev = node;
+        size++;
+    }
+};
+class LFUCache {
+public:
+    map<int,Node*> mp;
+    map<int,List*> freqmp;
+    int cap,mini,currsize;
+
     LFUCache(int capacity) {
-        this->capacity = capacity;
-        minFreq = 0;
+        cap=capacity; mini=0; currsize=0;
+    }
+
+    void f(Node* n){
+        mp.erase(n->key);
+        freqmp[n->freq]->deleteNode(n);
+
+        if(n->freq==mini && freqmp[n->freq]->size==0) mini++;
+
+        List* list;
+        if(freqmp.find(n->freq+1)!=freqmp.end()) list=freqmp[n->freq+1];
+        else list=new List();
+
+        n->freq+=1;
+        list->insertAfterHead(n);
+        freqmp[n->freq]=list;
+        mp[n->key]=n;
     }
     
     int get(int key) {
-        if(keyVal.find(key) == keyVal.end())
-            return -1;
-        freqList[keyVal[key].second].erase(pos[key]);
-        keyVal[key].second++;
-        freqList[keyVal[key].second].push_back(key);
-        pos[key] = --freqList[keyVal[key].second].end();
-        if(freqList[minFreq].empty())
-            minFreq++;
-        return keyVal[key].first;
+        if(mp.find(key)!=mp.end()){
+            Node* n=mp[key];
+            int ans=n->val;
+            f(n);
+            return ans;
+        }
+        return -1;
     }
     
     void put(int key, int value) {
-        if(!capacity)
-            return;
-        if(keyVal.find(key) != keyVal.end()) {
-            keyVal[key].first = value;
-            freqList[keyVal[key].second].erase(pos[key]);
-            keyVal[key].second++;
-            freqList[keyVal[key].second].push_back(key);
-            pos[key] = --freqList[keyVal[key].second].end();
-            if(freqList[minFreq].empty())
-                minFreq++;
-            return;
+        if(mp.find(key)!=mp.end()){
+            Node* n=mp[key];
+            n->val=value;
+            f(n);
         }
-        if(keyVal.size() == capacity) {
-            int delKey = freqList[minFreq].front();
-            keyVal.erase(delKey);
-            pos.erase(delKey);
-            freqList[minFreq].pop_front();
+        else{
+            if(currsize==cap){
+                List* list=freqmp[mini];
+                mp.erase(list->tail->prev->key);
+                freqmp[mini]->deleteNode(list->tail->prev);
+                currsize--;
+            }
+            mini=1; currsize++;
+
+            List* list;
+            if(freqmp.find(mini)!=freqmp.end()) list=freqmp[mini];
+            else list=new List();
+
+            Node* n=new Node(key,value);
+            list->insertAfterHead(n);
+            mp[key]=n; freqmp[mini]=list;
         }
-        keyVal[key] = {value,1};
-        freqList[1].push_back(key);
-        pos[key] = --freqList[1].end();
-        minFreq = 1;
     }
 };
-
-
 
 /**
  * Your LFUCache object will be instantiated and called as such:

ad4a432926b333a6df62370e9220d4de09834cea
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-07-31 12:35:23 +0530

    Time: 1521 ms (5.14%) | Memory: 338.6 MB (53.08%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int subarrayBitwiseORs(vector<int>& arr) {
-        vector<int> x;
-        set<int> st;
+        set<int> st,ans;
         for(auto it:arr){
-            set<int> ts;
-            ts.insert(it);
-            for(auto itr:st) ts.insert(it|itr);
+            set<int> ts; ts.insert(it);
+            for(auto itr:st) ts.insert(itr|it);
             st=ts;
-            for(auto itr:st) x.push_back(itr);
+            ans.insert(st.begin(),st.end());
         }
-        return set(x.begin(),x.end()).size();
+        return ans.size();
     }
 };

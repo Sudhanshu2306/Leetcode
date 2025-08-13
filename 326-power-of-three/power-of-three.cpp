d4665468279e04cb77276e74a104ca86401b5d9e
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-08-13 15:50:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (76.82%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -3,9 +3,7 @@ public:
     // bool flag=false;
     bool f(int n){
         if(n<=0) return false;
-        if(n==1){
-            return true;
-        }
+        if(n==1) return true;
 
         if(n%3==0) return f(n/3);
         return false;

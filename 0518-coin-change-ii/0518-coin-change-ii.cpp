c836816fd3f25adc66b91507cfe87c6eaf87d403
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-09-12 19:19:43 +0530

    Time: 47 ms (7.72%), Space: 81.8 MB (5.02%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-
-    int solve(int ind, int target, vector<int> &coins, vector<vector<int>> &dp){
+    // total number of ... jab bhi hoga, we will addd the possible combinations
+    int solve(int ind, int target, vector<int> &coins, vector<vector<long long>> &dp){
         
         if(ind==0) return (target%coins[0]==0);     
         
@@ -13,25 +13,48 @@ public:
         return dp[ind][target] = pick+notpick;
     }
     
-    int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));              
+    // int change(int amount, vector<int>& coins) {
+    //     int n=coins.size();
+    //     vector<vector<long long>> dp(n,vector<long long>(amount+1,-1));              
                               
-        // return solve(n-1,amount,coins,dp);
+    //     return solve(n-1,amount,coins,dp);
         
-        for(int i=0;i<=amount;i++){
-            dp[0][i]=(i%coins[0]==0);
-        }
+        // for(int i=0;i<=amount;i++){
+        //     dp[0][i]=1LL*(i%coins[0]==0);
+        // }
         
-        for(int i=1;i<n;i++){
-            for(int target=0;target<=amount;target++){
-                int notpick = dp[i-1][target];
-                int pick = 0;
-                if(coins[i]<=target) pick = dp[i][target-coins[i]] ;
+        // for(int i=1;i<n;i++){
+        //     for(int target=0;target<=amount;target++){
+        //         long long notpick = dp[i-1][target];
+        //         long long pick = 0;
+        //         if(coins[i]<=target) pick = dp[i][target-coins[i]] ;
 
-                dp[i][target] = pick+notpick;
+        //         dp[i][target] = pick+notpick;
+        //     }
+        // }
+        // // ye wahi return hota h jo hum memo waale function mein initially pass karte h
+        // return dp[n-1][amount];
+        
+    // }
+
+     int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<long double>> dp(n+1, vector<long double>(amount+1, 0LL));
+
+        
+        for(int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= amount; j++) {
+                if(coins[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
-        return dp[n-1][amount];
-    }
+
+        return dp[n][amount];
+    }
+
 };

e7424a275c5bf4227254e930f22ae0721196d51c
Sudhanshu Raj <126086429+Sudhanshu2306@users.noreply.github.com>
2025-12-18 12:38:52 +0530

    Time: 1815 ms (5.15%) | Memory: 515.5 MB (5.15%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
@@ -10,16 +10,16 @@ public:
 
         if(dp[i][k][x] !=-1e9) return dp[i][k][x];
 
-        ll ans=0;
+        ll ans=-1e9;
 
         if(x==0){
-            ans=max({f(i+1,1,k,prices)-prices[i],f(i+1,2,k,prices)+1LL*prices[i], f(i+1,0,k,prices)});
+            ans=max({ans,f(i+1,1,k,prices)-prices[i],f(i+1,2,k,prices)+1LL*prices[i], f(i+1,0,k,prices)});
         }
         else if(x==1){
-            ans=max({f(i+1,x,k,prices), f(i+1,0,k-1,prices)+1LL*prices[i]});
+            ans=max({ans,f(i+1,x,k,prices), f(i+1,0,k-1,prices)+1LL*prices[i]});
         }
         else{
-            ans=max({f(i+1,x,k,prices), f(i+1,0,k-1,prices)-1LL*prices[i]});
+            ans=max({ans,f(i+1,x,k,prices), f(i+1,0,k-1,prices)-1LL*prices[i]});
         }
 
         return dp[i][k][x]=ans;
